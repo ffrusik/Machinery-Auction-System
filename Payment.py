67cb68816fe323f215b197554ad1de0585c7b6b6
Ruslan Fefelov - STUDENT <l00189876@atu.ie>
2026-04-14 13:23:05 +0100

    Person and Payment classes setup

---
+++ Payment.py
@@ -0,0 +1,6 @@
+class Payment:
+    def __init__(self, auctionDate, siteNumber, buyerNumber, amountPaid):
+        self._auctionDate = auctionDate
+        self._siteNumber = siteNumber
+        self._buyerNumber = buyerNumber
+        self._amountPaid = amountPaid
--- Person.py
+++ Person.py
@@ -1,7 +1,34 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, phone, bankAccount, address, email):
         self._name = name
+        self._phone = phone
+        self._bankAccount = bankAccount
+        self._address = address
+        self._email = email
     def display(self):
-        return f"Name: {self._name}"
-    
+        return f"Name: {self._name}, Phone: {self._phone}, Bank Account: {self._bankAccount}, Address: {self._address}, Email: {self._email}"
 
+class Staff(Person):
+    def __init__(self, name, phone, bankAccount, address, email, position, salary, logonID, password, ppsNo):
+        super().__init__(name, phone, bankAccount, address, email)
+        self._position = position
+        self._salary = salary
+        self._logonID = logonID
+        self._password = password
+        self._ppsNo = ppsNo
+
+class Seller(Person):
+    def __init__(self, name, phone, bankAccount, address, email, sellerNo):
+        super().__init__(name, phone, bankAccount, address, email)
+        self._sellerNo = sellerNo
+
+class Buyer(Person):
+    def __init__(self, name, phone, bankAccount, address, email, buyerNo, vatNumber):
+        super().__init__(name, phone, bankAccount, address, email)
+        self._buyerNo = buyerNo
+        self._vatNumber = vatNumber
+
+class Auctioneer(Person):
+    def __init__(self, name, phone, bankAccount, address, email, auctionId):
+        super().__init__(name, phone, bankAccount, address, email)
+        self._auctionId = auctionId
