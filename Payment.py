84db48a3593e924aecc669606ed6a5f584124b90
Ruslan Fefelov - STUDENT <l00189876@atu.ie>
2026-04-16 12:21:06 +0100

    Changed variables to snake case

--- Payment.py
+++ Payment.py
@@ -1,6 +1,18 @@
 class Payment:
-    def __init__(self, auctionDate, siteNumber, buyerNumber, amountPaid):
-        self._auctionDate = auctionDate
-        self._siteNumber = siteNumber
-        self._buyerNumber = buyerNumber
-        self._amountPaid = amountPaid
+    def __init__(self, auction_date, site_number, buyer_number, amount_paid):
+        self._auction_date = auction_date
+        self._site_number = site_number
+        self._buyer_number = buyer_number
+        self._amount_paid = amount_paid
+
+    def get_auction_date(self):
+        return self._auction_date
+
+    def get_site_number(self):
+        return self._site_number
+
+    def get_buyer_number(self):
+        return self._buyer_number
+
+    def get_amount_paid(self):
+        return self._amount_paid
--- Person.py
+++ Person.py
@@ -1,27 +1,27 @@
 class Person:
-    def __init__(self, name, phone, bankAccount, address, email):
+    def __init__(self, name, phone, bank_account, address, email):
         self._name = name
         self._phone = phone
-        self._bankAccount = bankAccount
+        self._bank_account = bank_account
         self._address = address
         self._email = email
     def display(self):
-        return f"Name: {self._name}, Phone: {self._phone}, Bank Account: {self._bankAccount}, Address: {self._address}, Email: {self._email}"
+        return f"Name: {self._name}, Phone: {self._phone}, Bank Account: {self._bank_account}, Address: {self._address}, Email: {self._email}"
 
 class Staff(Person):
-    def __init__(self, name, phone, bankAccount, address, email, position, salary, logonID, password, ppsNo):
-        super().__init__(name, phone, bankAccount, address, email)
+    def __init__(self, name, phone, bank_account, address, email, position, salary, logon_id, password, pps_no):
+        super().__init__(name, phone, bank_account, address, email)
         self._position = position
         self._salary = salary
-        self._logonID = logonID
+        self._logon_id = logon_id
         self._password = password
-        self._ppsNo = ppsNo
+        self._pps_no = pps_no
 
 class Seller(Person):
     _sellerNo = 0
 
-    def __init__(self, name, phone, bankAccount, address, email):
-        super().__init__(name, phone, bankAccount, address, email)
+    def __init__(self, name, phone, bank_account, address, email):
+        super().__init__(name, phone, bank_account, address, email)
         self._sellerNo = Seller.generate_seller_id()
 
     @staticmethod
@@ -32,9 +32,9 @@ class Seller(Person):
 class Buyer(Person):
     _buyerNo = 0
 
-    def __init__(self, name, phone, bankAccount, address, email, vatNumber):
-        super().__init__(name, phone, bankAccount, address, email)
-        self._vatNumber = vatNumber
+    def __init__(self, name, phone, bank_account, address, email, vat_number):
+        super().__init__(name, phone, bank_account, address, email)
+        self._vatNumber = vat_number
         self._buyerNo = Buyer.generate_buyer_id()
 
     @staticmethod
@@ -45,7 +45,16 @@ class Buyer(Person):
     def display_user_info(self):
             print(f"User ID: {self._buyerNo}, Name: {self._name}")
 
+    def set_name(self, name):
+        self._name = name
+
+    def set_phone(self, phone):
+        self._phone = phone
+
+    def set_bank_account(self, bank_account):
+        self._bank_account = bank_account
+
 class Auctioneer(Person):
-    def __init__(self, name, phone, bankAccount, address, email, auctionId):
-        super().__init__(name, phone, bankAccount, address, email)
-        self._auctionId = auctionId
+    def __init__(self, name, phone, bank_account, address, email, auction_id):
+        super().__init__(name, phone, bank_account, address, email)
+        self._auction_id = auction_id
