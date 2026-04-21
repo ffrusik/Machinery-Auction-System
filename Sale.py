19eb650389ebe65c55b1067d72f2f7a3f4d76a81
Abdul-akeem1 <abdulkakson@gmail.com>
2026-04-21 01:15:28 +0100

    Initalise classes

---
+++ Sale.py
@@ -0,0 +1,10 @@
+class Sale:
+    def __init__(self, site_no, date, lot_no, buyer_no, amount, commission, vat, paid):
+        self.site_no = site_no
+        self.date = date
+        self.lot_no = lot_no
+        self.buyer_no = buyer_no
+        self.amount = amount
+        self.commission = commission
+        self.vat = vat
+        self.paid = paid
